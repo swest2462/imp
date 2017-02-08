1d3ee3c4a434ea38d2fba3f6976b8ca0ea42b749
Aleksandr Vest <swest2462@gmail.com>
2017-02-08 22:20:39 +0400

    some ads

--- imp.rb
+++ imp.rb
@@ -7,25 +7,12 @@ data_result = CSV.read("text.csv")
 data_result.each do |row|
   crosses = []
   (0..1).each do |t|
+    brand = t.row[1]
     html_doc = Nokogiri::HTML(row[t])
     table_rows = html_doc.search('tbody tr')
     make_name = table_rows[0].blank? ? '' : table_rows[0].text
     oem = table_rows[1].blank? ? '' : table_rows[1].text
-    crosses << [ make_name, oem ] # массив вида ["Производитель", "Номер"]
-  end
-
-  puts crosses
-
-end
-
-data_result.each do |row|
-  crosses = []
-  (2..3).each do |t|
-    html_doc = Nokogiri::HTML(row[t])
-    table_rows = html_doc.search('tbody tr')
-    make_name = table_rows[0].blank? ? '' : table_rows[0].text
-    oem = table_rows[1].blank? ? '' : table_rows[1].text
-    crosses << [ make_name, oem ] # массив вида ["Производитель", "Номер"]
+    crosses << [ make_name, oem, brand] # массив вида ["Производитель", "Номер"]
   end
 
   puts crosses
